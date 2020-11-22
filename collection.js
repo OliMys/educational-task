67af4f3e18acca220d6d917e54959d6335c1cb23
Olga <67907193+OliMys@users.noreply.github.com>
2020-11-23 00:41:10 +0300

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,71 @@
+/**
+ * @param {Array} collection
+ * @params {Function[]} – Функции для запроса
+ * @returns {Array}
+ */
+function query() {
+ var paramArr = [].slice.call(arguments);
+ var copyCol = paramArr[0].map((i) => Object.assign({}, i));
+ if(paramArr.length == 1 && Array.isArray(paramArr[0])) {
+     return copyCol;
+ } else {
+    var operations = [].slice.call(arguments, 1);
+    operations.sort();
+   //console.log(operations);
+   //operations[0](copyCol);
+   //operations[1](copyCol);
+   for (var operation of operations) {
+    (copyCol = operation(copyCol));
+   }
+   return copyCol;
+ } 
+ 
+}
+
+/**
+ * @params {String[]}
+ */
+function select() {
+    var fieldList = [].slice.call(arguments);
+return function selectKeys (collection) {
+    for(var elem of collection) {
+        var keys = Object.keys(elem);
+        for(var i of keys){
+            if(!fieldList.includes(i)) {
+                delete elem[i];
+            }
+        }
+    }
+    
+    return collection;
+}
+//retutn ['select', fieldList];
+}
+
+/**
+ * @param {String} property – Свойство для фильтрации
+ * @param {Array} values – Массив разрешённых значений
+ */
+function filterIn(property, values) {
+    var filterElems = [].slice.call(arguments);
+    var property = filterElems[0];
+    var values = filterElems[1];
+    return function filterValue(collection){
+        //var collectionArr = [].slice.call(collection);
+        var newCol = [];
+        collection.filter(function(elem) {
+            var currentPropertyValue = elem[property];
+            if(values.find((e) => e == currentPropertyValue)) {
+                newCol.push(elem);
+            }
+        })
+        return newCol;
+    }
+    //return ['filterIn', filterElems];
+}
+
+module.exports = {
+    query: query,
+    select: select,
+    filterIn: filterIn
+};
